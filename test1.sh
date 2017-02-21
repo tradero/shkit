1e877cc0407ac1c6dbe7001bb4b37bd65d2fc608
Krzysztof Antczak <x@tradero.net>
2017-02-21 12:24:47 +0100

    processing improvements

--- test1.sh
+++ test1.sh
@@ -4,6 +4,9 @@
 . array.subr
 . debug.subr
 . lines.subr
+. string.subr
+. escape.subr
+. grep.subr
 
 check_pf_line () {
 	check_line /etc/pf.conf "$1"
@@ -14,34 +17,25 @@ reset_pf () {
 	return
 }
 
-#reset_pf
+reset_pf
 clear;
 
 pf=/etc/pf.conf
 fix_eof_line $pf
 
-upsert_before $pf "ext_if=.*" 'ext_if="hn0"' "^(.*=|set|scrub|table|nat|rdr|pass|block)" "# INTERFACES\\
-ext_if=\"hn0\"\\
-\\
-"
+upsert_before $pf 'ext_if=.*' 'ext_if="hn0"' '^(.*=|set|scrub|table|nat|rdr|pass|block)' '# INTERFACES\next_if="hn0"\n\n\n'
 
-if [ $(check_pf_line 'nat on \$ext_if from \(\$jls_if:network\) to ! \(\$jls_if:network\) -> \(\$ext_if:0\)') -eq 0 ]; then
-	insert_after $pf "^(.*=.*|set|scrub|table(\n|.)*table)" "\\
-# NAT\\
-nat on \$ext_if from (\$jls_if:network) to ! (\$jls_if:network) -> (\$ext_if:0)\\
-nat on \$int_if from (\$jls_if:network) to ! (\$jls_if:network) -> (\$int_if:0)\\
-"
+if [ $(check_pf_line 'nat on $ext_if from ($jls_if:network) to ! ($jls_if:network) -> ($ext_if:0)') -eq 0 ]; then
+	insert_after $pf "^(.*=.*|set|scrub|table(\n|.)*table)" "$(join '\n'\
+		'\n# NAT'\
+		'nat on $ext_if from ($jls_if:network) to ! ($jls_if:network) -> ($ext_if:0)'\
+        	'nat on $int_if from ($jls_if:network) to ! ($jls_if:network) -> ($int_if:0)'\
+        )"
+
 fi
-	insert_after $pf "nat" "# /NAT\\
-"
-	insert_before $pf "rdr" "# REDIRECTS\\
-"
-	insert_after $pf "rdr" "# /REDIRECTS\\
-\\
-# PASS\\
-#\\
-"
-#fi
 
-cat /etc/pf.conf
+insert_after  $pf "nat" "# /NAT"
+insert_before $pf "rdr" "# REDIRECTS"
+insert_after  $pf "rdr" "# /REDIRECTS\n\n# PASS\n#"
+
 
