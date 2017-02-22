9ab2c4edb11d1b6de7552ce814d76139c98d6165
Krzysztof Antczak <x@tradero.net>
2017-02-22 12:11:37 +0100

    pkg + import

--- examples/test1.sh
+++ examples/test1.sh
@@ -11,20 +11,23 @@
 
 pf=/etc/pf.conf
 
-#reset_pf      $pf
+reset_pf      $pf
 fix_eof_line  $pf
 
 # file manipulation PoC
 
-upsert_before $pf 'ext_if=.*' 'ext_if="hn0"' '^(.*=|set|scrub|table|nat|rdr|pass|block)' '# INTERFACES\next_if="hn0"\n\n\n'
+iface="e"$(ifconfig | egrep "(: flag|inet )" | sed 'N;s/\n/ /' | egrep -v '(lo|vlan)[0-9]:' | sed 's/flags.*inet//' | awk '{ print $1 $2 }' | cut -d: -f1)
 
-if [ $(check_pf_line $(escape_regex 'nat on $ext_if from ($jls_if:network) to ! ($jls_if:network) -> ($ext_if:0)')) -eq 0 ]; then
+upsert_before $pf 'ext_if=.*' 'ext_if="'$iface'"' '^(.*=.*|set|scrub|table|nat|rdr|pass|block)' '# INTERFACES\next_if="'$iface'"\n\n\n'
+
+if [ $(check_pf_line 'nat on $ext_if from ($jls_if:network) to ! ($jls_if:network) -> ($ext_if:0)') -eq 0 ]; then
 	insert_after $pf "^(.*=.*|set|scrub|table(\n|.)*table)" "$(join '\n'\
 		'\n# NAT'\
 		'nat on $ext_if from ($jls_if:network) to ! ($jls_if:network) -> ($ext_if:0)'\
         	'nat on $int_if from ($jls_if:network) to ! ($jls_if:network) -> ($int_if:0)'\
         )"
-
+else
+#	echo "skipping nat settings - already set"
 fi
 
 insert_after  $pf "nat" "# /NAT"
---
+++ examples/test2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+. shkit.sh
+#
+
+import pkg    \
+       debug  \
+       lines  \
+       string
+
+pkg install libssh2
+
